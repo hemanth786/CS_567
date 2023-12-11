2a1de118e5a8c1f74215b6dfe73f4110b4e6e967
HEMANTH SAI KUMAR <hemanthsaikumar786@gmail.com>
2023-12-10 20:04:31 -0700

    Update testcode.py

--- testcode.py
+++ testcode.py
@@ -1,47 +1,85 @@
-# library_system.tstl
+# testcode.py
+import unittest
+from code import Book, Patron, Library
+from datetime import datetime, timedelta
 
-# Include the necessary modules
-include "code.py"
+class TestLibraryManagementSystem(unittest.TestCase):
+    def setUp(self):
+        self.library = Library()
 
-# Declare variables for the library, books, and patrons
-library = Library()
-book1 = Book("The Great Gatsby", "F. Scott Fitzgerald", "978-0743273565", copies=3)
-book2 = Book("To Kill a Mockingbird", "Harper Lee", "978-0061120084", copies=2)
-patron1 = Patron("Alice", 1)
-patron2 = Patron("Bob", 2)
+        self.book1 = Book("The Great Gatsby", "F. Scott Fitzgerald", "978-0743273565", copies=3)
+        self.book2 = Book("To Kill a Mockingbird", "Harper Lee", "978-0061120084", copies=2)
 
-# Add books and patrons to the library
-library.add_book(book1)
-library.add_book(book2)
-library.add_patron(patron1)
-library.add_patron(patron2)
+        self.library.add_book(self.book1)
+        self.library.add_book(self.book2)
 
-# Test check_out and check_in functions
-library.check_out("The Great Gatsby", 1)
-library.check_out("To Kill a Mockingbird", 2)
-library.check_in("The Great Gatsby")
-library.check_in("To Kill a Mockingbird")
+        self.patron1 = Patron("Alice", 1)
+        self.patron2 = Patron("Bob", 2)
 
-# Test display_available_books function
-library.display_available_books()
+        self.library.add_patron(self.patron1)
+        self.library.add_patron(self.patron2)
 
-# Test search_books function
-library.search_books("Great")
+    def test_checkout_book(self):
+        due_date1 = datetime.now() + timedelta(days=14)
+        due_date2 = datetime.now() + timedelta(days=7)
 
-# Test checkout_book and checkin_book functions with due dates
-library.checkout_book("The Great Gatsby", 1, datetime.now() + timedelta(days=14))
-library.checkout_book("The Great Gatsby", 2, datetime.now() + timedelta(days=7))
-library.checkout_book("To Kill a Mockingbird", 1, datetime.now() + timedelta(days=14))
-library.checkin_book("The Great Gatsby", 1)
-library.checkin_book("To Kill a Mockingbird", 2)
+        self.assertEqual(self.library.checkout_book("The Great Gatsby", 1, due_date1),
+                         "The Great Gatsby checked out successfully by Patron 1.")
+        self.assertEqual(self.library.checkout_book("The Great Gatsby", 2, due_date2),
+                         "The Great Gatsby checked out successfully by Patron 2.")
+        self.assertEqual(self.library.checkout_book("To Kill a Mockingbird", 1, due_date1),
+                         "To Kill a Mockingbird checked out successfully by Patron 1.")
+        self.assertEqual(self.library.checkout_book("Nonexistent Book", 1, due_date1),
+                         "Error: Nonexistent Book does not exist in the library.")
 
-# Test reserve_book function
-library.reserve_book("The Great Gatsby", 1)
-library.reserve_book("The Great Gatsby", 2)
-library.reserve_book("Nonexistent Book", 1)
+    def test_checkin_book(self):
+        due_date1 = datetime.now() + timedelta(days=14)
+        self.library.checkout_book("The Great Gatsby", 1, due_date1)
+        self.library.checkout_book("To Kill a Mockingbird", 2, due_date1)
 
-# Test overdue_books function
-library.overdue_books()
+        self.assertEqual(self.library.checkin_book("The Great Gatsby", 1),
+                         "The Great Gatsby checked in successfully by Patron 1.")
+        self.assertEqual(self.library.checkin_book("To Kill a Mockingbird", 2),
+                         "To Kill a Mockingbird checked in successfully by Patron 2.")
 
-# Test get_popular_books function
-library.get_popular_books()
+    def test_reserve_book(self):
+        self.assertEqual(self.library.reserve_book("The Great Gatsby", 1),
+                         "The Great Gatsby reserved successfully by Patron 1.")
+        self.assertEqual(self.library.reserve_book("The Great Gatsby", 2),
+                         "Sorry, The Great Gatsby is reserved by another patron.")
+        self.assertEqual(self.library.reserve_book("Nonexistent Book", 1),
+                         "Error: Nonexistent Book does not exist in the library.")
+
+    def test_display_available_books(self):
+        available_books = self.library.display_available_books()
+        self.assertEqual(len(available_books), 2)
+        self.assertEqual(available_books[0].title, "The Great Gatsby")
+
+    def test_search_books(self):
+        search_results = self.library.search_books("Great")
+        self.assertEqual(len(search_results), 1)
+        self.assertEqual(search_results[0].title, "The Great Gatsby")
+
+    def test_overdue_books(self):
+        due_date1 = datetime.now() - timedelta(days=7)
+        due_date2 = datetime.now() - timedelta(days=14)
+
+        self.library.checkout_book("The Great Gatsby", 1, due_date1)
+        self.library.checkout_book("To Kill a Mockingbird", 2, due_date2)
+
+        overdue_books = self.library.overdue_books()
+        self.assertEqual(len(overdue_books), 2)
+        self.assertIn("The Great Gatsby is overdue for Alice (ID: 1). Due date was", overdue_books[0])
+        self.assertIn("To Kill a Mockingbird is overdue for Bob (ID: 2). Due date was", overdue_books[1])
+
+    def test_get_popular_books(self):
+        self.library.checkout_book("The Great Gatsby", 1, datetime.now() - timedelta(days=10))
+        self.library.checkout_book("The Great Gatsby", 2, datetime.now() - timedelta(days=5))
+        self.library.checkout_book("To Kill a Mockingbird", 1, datetime.now() - timedelta(days=3))
+
+        popular_books = self.library.get_popular_books(num_books=1)
+        self.assertEqual(len(popular_books), 1)
+        self.assertIn("The Great Gatsby", popular_books[0])
+
+if __name__ == "__main__":
+    unittest.main()
